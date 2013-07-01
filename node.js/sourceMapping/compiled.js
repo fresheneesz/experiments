4bebe7f23dc3d0f283675c193930474cc2f30ea6
unknown <btetrud@BTETRUD-D54587.corp.playdom.com>
2013-07-01 15:40:36 -0700

    source maps work in intellij!!!

--- node.js/sourceMapping/compiled.js
+++ node.js/sourceMapping/compiled.js
@@ -2,3 +2,9 @@
 require('source-map-support').install();
 
 throw new Error("test");
+
+// This format works in intelliJ!!!!
+// Note that intelliJ does *not* recognize # but does recognize single // comments
+/*
+//@ sourceMappingURL=original.sourcemap.json
+*/
