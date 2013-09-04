b5e811ceec4dbb505ea9335efc9b259e5cf68daa
fresheneesz <bitetrudpublic@gmail.com>
2013-09-04 10:02:27 -0700

    changes

--- js/cordova/hello/www/require.config.js
+++ js/cordova/hello/www/require.config.js
@@ -1,13 +1,15 @@
 require.config({
     paths: {
         "jquery": "jquery",
+        "jquery.mobile": "http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.js",
         "createjs.tween": "http://code.createjs.com/tweenjs-0.4.1.min",
-        'jquery.easing': "jquery.easing.1.3"
+        "jquery.easing": "jquery.easing.1.3"
     },
     shim: {
         'jquery-animate': {deps: ["jquery"]},
         'jquery-transit': {deps: ["jquery"]},
         'jquery.easing.1.3': {deps: ["jquery"]},
-        'zepto': {deps: ["jquery"]}
+        'zepto': {deps: ["jquery"]},
+        'jquery.mobile': {deps: ["jquery"]}
     }
 })
